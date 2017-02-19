5321f3593eb609dad58b79f0e51f84e5ec1bd263
Steven Rodriguez <sgrod91@gmail.com>
2017-02-19 16:13:36 -0500

    Update my_project.py

--- my_project.py
+++ my_project.py
@@ -124,29 +124,34 @@ while menu_choice != 9:
     elif menu_choice == 2:
         for song in songs:
             print song['title']
-    #elif menu_choice == 3:
-        #print playlist_name
+    elif menu_choice == 3:
+        print playlists
     elif menu_choice == 4:
-        playlist_choice_answer = 0
-        while playlist_choice_answer != "N":
-            playlist_name = raw_input('Name your playlist: ' )
+        def playlist_name():
+            raw_input('Name your playlist: ' )
             song_choice = raw_input('Type the name of the song you wish to add: ')
             for song in songs:
                 if song['title'] == song_choice:
-                    playlist_name.append
+                    playlists.append(song_choice)
                     print 'song added'
-            playlist_choice_answer = raw_input('Would you like to add another? (Y or N) ')
-    #elif menu_choice == 5:
-        #smart_playlist_name = raw_input('Name your smart playlist: ')
-        #playist_count = raw_input('How many songs in your playlist? ')
-        #favorite_decade = raw_input('Favorite decade? (60s, 70s, 80s, 90s, 00s, 10s) ')
-        #favorite_genre = raw_input('Favorite genre? ')
+            #add_another = raw_input('Would you like to add another? (Y or N) ')
+            #if add_another == "Y":
+                #playlist_name()
+            #elif add_another == "N":
+                #print_menu()
+
+    elif menu_choice == 5:
+        smart_playlist_name = raw_input('Name your smart playlist: ')
+        playist_count = raw_input('How many songs in your playlist? ')
+        favorite_decade = raw_input('Choose a decade: 60s, 70s, 80s, 90s, 00s, 10s) ')
+        favorite_genre = raw_input('Choose a genre ')
     elif menu_choice == 6:
         song_title = raw_input('Song title?: ')
         artist_name = raw_input('Artist name?: ')
         genre = raw_input('genre?: ')
         year = int(raw_input('year?: '))
-        songs.extend(song_title, artist_name, genre, year)
+        new_song = {song_title, artist_name, genre, year}
+        songs.extend(new_song)
         print "song added"
     elif menu_choice == 7:
         delete_song = raw_input('What song do you want to delete?')
